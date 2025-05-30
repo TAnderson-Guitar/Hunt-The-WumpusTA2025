e2d5eca30b39eff399d3a289137a3bb8ecf5bdb3
TAnderson-Guitar <toby.anderson9@education.nsw.gov.au>
2025-05-30 00:41:51 +0000

    Added more methods to the Cave class

--- cave.py
+++ cave.py
@@ -1,4 +1,19 @@
 class Cave:
     def __init__(self, cave_name):
-        self.name = None
+        self.name = cave_name
         self.description = None
+
+    def set_name(self, cave_name):
+        self.name = cave_name
+
+    def get_name(self):
+        return self.name
+    
+    def set_description(self, cave_description):
+        self.description = cave_description
+    
+    def get_description(self):
+        return self.description
+    
+    def describe(self):
+        print(self.description)
