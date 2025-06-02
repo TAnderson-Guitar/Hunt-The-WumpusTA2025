0d0eae5ece774e024d4e50e63f42e5c378dfe358
TAnderson-Guitar <toby.anderson9@education.nsw.gov.au>
2025-06-02 01:05:38 +0000

    Establihsed the link between caves and added a journal file.

--- cave.py
+++ cave.py
@@ -2,7 +2,7 @@ class Cave:
     def __init__(self, cave_name):
         self.name = cave_name
         self.description = None
-        self.linked_caves = None
+        self.linked_caves = {}
 
     def set_name(self, cave_name):
         self.name = cave_name
@@ -18,3 +18,11 @@ class Cave:
     
     def describe(self):
         print(self.description)
+
+    def link_caves(self, cave_to_link, direction):
+        self.linked_caves[direction] = cave_to_link
+        #print(self.name + "linked caves:" + repr(self.linked_caves))
+
+    def get_details(self):
+        for direction, cave in self.linked_caves.items():
+            print("The " + cave.get_name() + " is " + direction)
