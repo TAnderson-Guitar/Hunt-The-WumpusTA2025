f8df133f4e0e41d3502e7d2e95c769eb2083a3dc
TAnderson-Guitar <toby.anderson9@education.nsw.gov.au>
2025-06-04 01:21:32 +0000

    Modified the enemy subclass and allowed the player to fight with the objects, created weakness to fend off the wumpus as well

--- char_test.py
+++ char_test.py
@@ -5,4 +5,7 @@ harry = Enemy("Harry", "A dirty smelly Wumpus")
 harry.describe()
 harry.set_conversation("Come closer. I cannot see you.")
 harry.talk()
-print(harry)
+harry.set_weakness("vegemite")
+
+fight_with = input("What do you want to fight with?")
+harry.fight(fight_with)
--- character.py
+++ character.py
@@ -29,6 +29,24 @@ class Character:
         return True
 
 class Enemy(Character):
+    """Defines attributes and methods for Enemy objets"""
     def __init__(self, char_name, char_description):
         super().__init__(char_name, char_description)
-        
+        self.weakness = None
+
+    def set_weakness(self, item_weakness):
+        """Sets the weakness for the Enemy class"""
+        self.weakness = item_weakness
+
+    def get_weakness(self):
+        """Gets the weakness for the Enemy class"""
+        return self.weakness
+
+    def fight(self, combat_item):
+        "Method to fight the Wumpus"
+        if combat_item == self.weakness:
+            print("You fend off " + self.name + " with the " + combat_item)
+            return True
+        else:
+            print(self.name + " swallows you whole! You died.")
+            return False
